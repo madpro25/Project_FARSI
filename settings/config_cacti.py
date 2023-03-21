a79554e94b740cc84172d67c36f5ab264b9bef4f
madpro25 <aaditya2002pramod@gmail.com>
2023-03-21 10:08:39 +0530

    Added cacti files and configured environment

--- settings/config_cacti.py
+++ settings/config_cacti.py
@@ -1,4 +1,4 @@
-cact_bin_addr = "/home/reddi-rtx/FARSI_related_stuff/Project_FARSI_TECS/Project_FARSI_6/cacti_for_FARSI/cacti"
-cacti_param_addr = "/home/reddi-rtx/FARSI_related_stuff/Project_FARSI_TECS/Project_FARSI_6/cacti_for_FARSI/farsi_gen.cfg"
-cacti_data_log_file = "/home/reddi-rtx/FARSI_related_stuff/Project_FARSI_TECS/Project_FARSI_6/cacti_for_FARSI/data_log.csv"
+cact_bin_addr = "/home/aaditya/Project_FARSI/cacti_for_FARSI/cacti"
+cacti_param_addr = "/home/aaditya/Project_FARSI/cacti_for_FARSI/farsi_gen.cfg"
+cacti_data_log_file = "/home/aaditya/Project_FARSI/cacti_for_FARSI/data_log.csv"
 
